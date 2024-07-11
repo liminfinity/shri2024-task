5104b40b7c84185aef07bc0f0f7d88a4d603f833
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-07-12 01:02:45 +0700

    upgraded to 110 point

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import {createRoot} from 'react-dom/client'
 import Main from './components/Main.jsx'
 import { StrictMode } from 'react'
-import "./styles/reset.css"
-import "./styles/styles.css"
 import Header from './components/Header.jsx'
 
 
